8d31634c3f02756758f407e2fd3dc3b5032bb64f
uncleserge <serge@sadowsky.spb.ru>
2023-08-17 01:05:08 +0300

    debounce function implemented

--- m3t2/js/script.js
+++ m3t2/js/script.js
@@ -1,12 +1,20 @@
 {
-  const myInput = document.querySelector('#myInput');
-  const myOutput= document.querySelector('#myOutput');
+    function debounce(func, timeout ){
+        let timer;
+        return (...args) => {
+        clearTimeout(timer);
+        console.log(...args);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+        };
+    }
+    
+    function saveInput(e, o){
+        o.textContent = e.target.value;
+    }
 
-  const outputFunc = (text) => {
-    myOutput.textContent = text;
-  };
-  
-  myInput.addEventListener('input', e => {    
-    setTimeout(outputFunc, 300, e.target.value);
-  });
+    const myOutput= document.querySelector('#myOutput');
+    const processor = debounce((e) => saveInput(e, myOutput), 300);
+
+    const myInput = document.querySelector('#myInput');
+    myInput.addEventListener('input',processor);
 }
